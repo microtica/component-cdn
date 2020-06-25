b05dd7aa3a4d43a4b415247637e816227b136d1f
radedox <rade@microtica.com>
2020-06-25 09:11:58 +0200

    initial commit

---
+++ infra/index.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "website-bucket" {
+  bucket = "my-website-bucket-11234565432"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}

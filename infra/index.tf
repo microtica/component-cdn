368dbd92f53e9496ffafadeefca1c21d9fe73d34
Rade Despodovski <rade@microtica.com>
2020-07-29 12:27:05 +0000

    index.tf edited online with Bitbucket

--- infra/index.tf
+++ infra/index.tf
@@ -3,6 +3,24 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+
+resource "aws_s3_bucket" "website-bucket" {
+  bucket = "my-website-bucket-tf-state-11234565433"
+  
+  versioning {
+    enabled = true
+  }
+}
+
+terraform {
+ backend "s3" {
+   encrypt = true
+   bucket = "my-website-bucket-tf-state-11234565433"
+   region = eu-central-1
+   key = state
+ }
+}
+
 resource "aws_s3_bucket" "website-bucket" {
   bucket = "my-website-bucket-11234565433"
   acl    = "public-read"
